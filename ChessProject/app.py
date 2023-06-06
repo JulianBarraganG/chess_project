f58487963fad7fdac6cf4ec679d07453115fa40d
OskarLinke <olinke2@gmail.com>
2023-06-06 17:50:10 +0200

    added openings

--- ChessProject/app.py
+++ ChessProject/app.py
@@ -1,17 +1,24 @@
 from flask import Flask, render_template, request
 import main
 import pgntofen
+import sql 
 
 pgnConverter = pgntofen.PgnToFen()
 start_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
 app = Flask(__name__)
+opning = sql.unique
 
 #Herinde kigger vi på hvad der sker i inputfeltet
 @app.route('/', methods=['GET', 'POST'])
 def home():
+
     if request.method == 'POST':
         #request.form['move'] er det som er skrevet i inputfeltet
         move = request.form['move']
+        opening = request.form['selectOpening']
+
+
+        print(opening)
 
         #lige nu laver den en fast position uanset input
         #der mangler altså at den husker hvilken position vi står i (den seneste der blev sendt til brættet)
@@ -23,15 +30,16 @@ def home():
         #her konverterer vi listen af pgn notation (fås fra databasen) til fen-notation
         newBoard = pgnConverter.moves('1. d4 Nf6 2. c4 e6 3. g3 d5 4. Bg2 dxc4').getFullFen()
     
-        print(newBoard)
         #her kalder vi den funktion som i sidste ende gør, at brættet bliver vist på skærmen
         main.main(newBoard)
+        
+        
+    return render_template('index.html', openings = opning)
 
-
-    return render_template('index.html')
 
 #det her er bare det allerførste der sker når man kører appen
 if __name__ == '__main__':
     #brættet bliver sat til at vise startpositionen i udgangspunktet
     main.main(start_fen)
+    
     app.run()
