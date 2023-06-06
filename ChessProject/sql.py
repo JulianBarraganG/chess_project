8eb3cb6316a145c9c89ddb0e75a9102b7e4e3cf5
JulianBarraganG <xjulianbarragan@gmail.com>
2023-06-06 17:43:03 +0200

    cleaned up SQL and commented code

--- ChessProject/sql.py
+++ ChessProject/sql.py
@@ -2,14 +2,17 @@ import psycopg2
 from psycopg2 import sql
 from pgntofen import *
 
+#TO BE MODIFIED
+YOUR_PASSWORD = 'type your password here'
+YOUR_PORT = '4 digit por'
 
-# Establish a connection to the database. Make sure to change stringnames to your server.
+# Establish a connection to server (that you setup yourself). OBS! Check that port, password etc. are correct.
 conn = psycopg2.connect(
     host="localhost",
     database="postgres",
     user="postgres",
-    password="vxz73ptw",
-    port="5432"
+    password= YOUR_PASSWORD,
+    port= YOUR_PORT
 )
 
 # Create a cursor object
@@ -26,11 +29,15 @@ create_table_sql = """
 
 cur.execute(create_table_sql)
 
+# The 5 file names of the tsv-files
 files = ['a','b','c','d','e']
+
+# INSERT HERE -- directory filepath to and with Openings. i.g. below.
+your_path = "C:\\Users\\hulig\\OneDrive - University of Copenhagen\\ML\\DIS\\ChessProject\\Openings\\" #remember double backslashes
 
 # Read the TSV file
 for file in files:
-    with open(f'C:\\Users\\Kaest\\Documents\\KU\\2_Semester\\DIS\\Projekt\\chess_project\\ChessProject\\Openings\\{file}.tsv', 'r') as f:
+    with open(your_path+f"{file}.tsv", 'r') as f:
         next(f)  # Skip the header line
         for line in f:
             data = line.strip().split('\t')
@@ -49,7 +56,6 @@ for file in files:
 
 
 # Fetch all rows from the result set
-
 select_rows_sql = """
     SELECT *
     FROM Openings;
@@ -57,8 +63,7 @@ select_rows_sql = """
 
 unique_openings = """
     SELECT DISTINCT opening
-    FROM Openings
-    WHERE variation IS NULL;
+    FROM Openings;
 """
 
 # Queries the pgn for every variation of a given opening
@@ -79,12 +84,12 @@ def listOfVars(opening_name: str) -> list:
         lst.append(PgnToFen.pgnToStringList(query[i][0]))
     return lst
 
-def get_unique():
+# Query every unique opening as a list of strings
+def get_unique_openings():
     cur.execute(unique_openings)
-    return (cur.fetchall())
-
-
-print(get_unique())
+    result = cur.fetchall()
+    unique_openings_list = [row[0] for row in result]
+    return unique_openings_list
 
 # Commit the changes and close the cursor and connection
 conn.commit()
