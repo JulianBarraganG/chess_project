73c81e7b662908f09c3ea8c699e724f532b9d0a0
OskarLinke <olinke2@gmail.com>
2023-06-06 23:01:42 +0200

    made it so that the two submit buttons submit different things

--- ChessProject/app.py
+++ ChessProject/app.py
@@ -8,36 +8,35 @@ start_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
 app = Flask(__name__)
 opning = sql.unique
 
+
+@app.route('/')
+def home(): 
+    return render_template('index.html', openings = opning)
+
 #Herinde kigger vi på hvad der sker i inputfeltet
-@app.route('/', methods=['GET', 'POST'])
-def home():
+@app.route('/move', methods=['GET','POST'])
+def moving():
+    print(main.currentBoard)
+    if request.method == 'POST': 
+        move = request.form['move']
+        next_move = main.currentBoard
+        next_move = next_move.__add__([move])
+        print(next_move)
+    return render_template('index.html', openings = opning)
+        
 
+@app.route('/opening', methods=['GET','POST'])
+def pick_opening(): 
     if request.method == 'POST':
-
-
         newBoard = pgnConverter.resetBoard()
         #request.form['move'] er det som er skrevet i inputfeltet
-        move = request.form['move']
         opening = request.form['selectOpening']
-
-
-        print(opening)
         pgn_of_opening = sql.get_specific_opening(opening)[0][0]
-
-        #lige nu laver den en fast position uanset input
-        #der mangler altså at den husker hvilken position vi står i (den seneste der blev sendt til brættet)
-        #den skal så lave an query baseret på den position og inputtet, og returnere en pgn fra databasen hvis det er et korrekt træk
-        #eller skrive på skærmen at det var forkert, hvis det var et forkert træk
-        #Lige nu crasher alt også hvis man spiller et træk den ikke kan forstå. Forhåbentligt bliver det også fikset når de eneste pgn-træk der bliver
-        #sendt til pgnConverter.moves er dem som ligger inde i databasen, fordi den kun skal sende dem afsted, som matcher noget i databasen. 
-
-        #her konverterer vi listen af pgn notation (fås fra databasen) til fen-notation
-        newBoard = pgnConverter.moves(pgn_of_opening).getFullFen()
-    
+        string_of_moves = pgnConverter.pgnToStringList(pgn_of_opening)
+        main.currentBoard = string_of_moves
+        newBoard = pgnConverter.moves(string_of_moves).getFullFen()
         #her kalder vi den funktion som i sidste ende gør, at brættet bliver vist på skærmen
         main.main(newBoard)
-        
-        
     return render_template('index.html', openings = opning)
 
 
