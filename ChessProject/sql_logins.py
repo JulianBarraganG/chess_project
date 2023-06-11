72aca54f511c7b5ef16a8a5e1f04a283f30d33cb
OskarLinke <olinke2@gmail.com>
2023-06-11 15:58:18 +0200

    whops

--- ChessProject/sql_logins.py
+++ ChessProject/sql_logins.py
@@ -35,23 +35,51 @@ def create_users_table():
 
 # er vel en many-to-many... en user kan have mange favorite openings, 
 # og openings kan være favorite af mange users.
-# bruger pgn som key, og lave hjælpe-insertfunktion til at tilføje navn
+# bruger et nyoprettet id som primary key
+#checker at der ikke er user_id, opening_pgn duplicates (altså at en user kun kan have en bestemt åbning favorited 1 gang)
+
 create_fav_open_relation = """
     DROP TABLE IF EXISTS Fav_Open;
     CREATE TABLE Fav_Open (
-        userID INT PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
+        userID INT,
         opening_pgn VARCHAR,
         opening_name VARCHAR,
         
         CONSTRAINT fk_openings FOREIGN KEY (opening_pgn) REFERENCES Openings(pgn),
-        CONSTRAINT fk_userID FOREIGN KEY (userID) REFERENCES Users(id)
+        CONSTRAINT fk_userID FOREIGN KEY (userID) REFERENCES Users(id),
+        CONSTRAINT only_unique_openings_per_user UNIQUE (userID, opening_pgn)
     );
     """
+
+
+#Benytter Julians arbejdsgang fra create_users_table til at oprette tablet kun hvis det ikke allerede findes. 
+def create_fave_openings_table():
+    """
+    This function creates the fave openings table, if it doesn't already exist in the database.
+    This ensures that the database keeps all the fave openings in the table.
+    """
+    # Establish connection
+    cur = sql.conn.cursor()
+
+    # Check if the table exists
+    cur.execute("SELECT EXISTS(SELECT * FROM information_schema.tables WHERE table_name = 'fav_open');")
+    table_exists = cur.fetchone()[0]
+    print(table_exists)
+
+    if not table_exists:
+        print("Creating table from scratch")
+        # Create the table if it doesn't exist
+        cur.execute(create_fav_open_relation)
+        sql.conn.commit()
+    else:
+        print("We already have this table")
+    cur.close()
 
 #intentionen er at lave en funktion der vil indsætte fav opening,
 # med alle kolloner udfyldt.
 def InsertOpenFavOpen(user:int,pgn:str):
-    #get name for the insert
+    #get name for the insert  
     get_name_query = f"""
     SELECT DISTINCT opening
     FROM Openings
@@ -68,6 +96,6 @@ def InsertOpenFavOpen(user:int,pgn:str):
     
     return insert_query
 
-
+create_fave_openings_table()
 #cur.execute(create_fav_open_relation)
 sql.conn.commit()
